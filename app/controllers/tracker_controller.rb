e9518a8c026452a52290514087bf5ffc473b3426
Toshimasa Ishibashi <iandeth@gmail.com>
2016-10-12 18:21:23 +0900

    writer side working

--- app/controllers/tracker_controller.rb
+++ app/controllers/tracker_controller.rb
@@ -2,12 +2,68 @@ class TrackerController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    expires_in 1.day, :public => true
-    headers['ETag'] = "Wooo"
-    render :js => %q|alert("hello2");|
+    writer_page = "//client-test.s3.amazonaws.com/iandeth/20161012-etag/"
+    ref = (request.headers['HTTP_REFERER'] || "")
+
+    if ref.match(%r|^https?:#{writer_page}|)
+      # 書き込み側からのリクエストだった場合は etag 発行 + stored data 作成
+      data = 1
+      etag = generate_etag()
+      file = save_data(etag, data)
+      render_response(etag, data, file.mtime.httpdate)
+    else
+      # 読み込み側ページからのリクエストの場合は stored data を読み取って返す
+      etag = retrieve_etag_from_header()
+      ret = load_data(etag)
+      if ret.present?
+        render_response(etag, ret[:data], ret[:last_modified])
+      else
+        data = 0
+        etag = generate_etag()
+        render_response(etag, data)
+      end
+    end
   end
 
   private
+  # etag related
+  def generate_etag
+    SecureRandom.hex(14)
+  end
+
+  def retrieve_etag_from_header()
+    (request.headers["HTTP_IF_NONE_MATCH"] || "").gsub(/"/, '')
+  end
+
+  def render_response(etag, data, last_modified=Time.zone.now.httpdate)
+    expires_in 20.seconds, :public => true
+    headers['ETag'] = %Q|"#{etag}"|
+    headers['Last-Modified'] = last_modified
+    render :js => %Q|var kz = { is_login_user: #{data} };|
+  end
+
+  # store data related
+  def save_data(etag, data=0)
+    raise "etag is blank" if etag.blank?
+    f = _get_file_path(etag)
+    FileUtils.mkpath(f.dirname) unless f.dirname.directory?
+    File.open(f, 'w') {|fh| fh.write YAML.dump(data) }
+    f
+  end
+
+  def load_data(etag)
+    return if etag.blank?
+    f = _get_file_path(etag)
+    return unless f.exist?
+    data = YAML.load_file(f)
+    { data: data, last_modified: f.mtime.httpdate }
+  end
+
+  def _get_file_path(etag)
+    head = etag[0,4]
+    Rails.configuration.storage[:root_dir] + "#{head}/#{etag}.yml"
+  end
+
   #def caching_allowed?
   #  false
   #end
--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,6 @@ module StaffserviceEtag
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.storage = { root_dir: Rails.root + "db/etags" }
   end
 end
